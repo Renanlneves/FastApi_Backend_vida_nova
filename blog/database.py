d98b5e552ffe553485e9396fee3b34e06faf65c0
Renan Neves <renanlneves@gmail.com>
2022-10-31 11:45:05 -0300

    criada conexão com banco de dados

--- blog/database.py
+++ blog/database.py
@@ -1,3 +1,12 @@
+from re import S
 from sqlalchemy import create_engine, engine
+from sqlalchemy.orm import declarative_base
+from sqlalchemy.orm import sessionmaker
 
-engine = create_engine ('sqlite:///:memory:', echo=True)
+SQLALCHEMY_DATABASE_URL = 'sqlite:///./blog.db'
+
+engine = create_engine (SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
+
+Base = declarative_base
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
