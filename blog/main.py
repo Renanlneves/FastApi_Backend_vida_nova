546c2a64c9e37abbc44e89800cad44a0fc69ac88
Priscila Neves <kpriscilaneves@gmail.com>
2022-11-01 16:03:26 -0300

    armazenando dados no banco de dados

--- blog/main.py
+++ blog/main.py
@@ -1,18 +1,32 @@
-from fastapi import FastAPI
+from turtle import title
+from fastapi import FastAPI, Depends
 import uvicorn
+from config.database import SessionLocal
 from domain.account import schemas
 from domain.account import models
-from config.database import engine
+from config.database import engine, SessionLocal
+from sqlalchemy.orm import Session
 
 app = FastAPI()
 
 
 models.Base.metadata.create_all(bind=engine)
 
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
 
 @app.post("/blog")
-def create(request: schemas.Blog):
-    return {request}
+def create(request: schemas.Blog, db: Session = Depends(get_db)):
+    new_blog = models.Blog(title=request.title, body=request.body)
+    db.add(new_blog)
+    db.commit()
+    db.refresh(new_blog)
+    return new_blog
 
 
 if __name__ == "__main__":
