9daed5e0145d0bb52abb548ea5ff5882aad11a4c
Priscila Neves <kpriscilaneves@gmail.com>
2022-11-02 13:19:04 -0300

    pegando dados

--- blog/main.py
+++ blog/main.py
@@ -20,13 +20,25 @@ def get_db():
         db.close()
 
 
-@app.post("/blog")
+@app.post("/blog", status_code= 201)
 def create(request: schemas.Blog, db: Session = Depends(get_db)):
     new_blog = models.Blog(title=request.title, body=request.body)
     db.add(new_blog)
     db.commit()
     db.refresh(new_blog)
     return new_blog
+
+
+@app.get("/blog") #pegando todos os dados 
+def all(db: Session = Depends(get_db)):
+    blogs = db.query(models.Blog).all()
+    return blogs
+
+
+@app.get("/blog/{id}") #pegando um determinado dados pela id
+def show(id, db: Session = Depends(get_db)):
+    blog = db.query(models.blog).filter(models.Blog.id == id).first # filter funciona como um "where" do sql / first significa que queremos apenas o primeiro
+    return blog
 
 
 if __name__ == "__main__":
