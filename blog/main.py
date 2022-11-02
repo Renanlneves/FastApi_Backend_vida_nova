9c574df9fa42c598909db92a0759d30fdac204ac
Priscila Neves <kpriscilaneves@gmail.com>
2022-11-02 17:28:59 -0300

    update criado

--- blog/main.py
+++ blog/main.py
@@ -1,5 +1,4 @@
-from turtle import title
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, status, Response, HTTPException
 import uvicorn
 from config.database import SessionLocal
 from domain.account import schemas
@@ -20,7 +19,7 @@ def get_db():
         db.close()
 
 
-@app.post("/blog", status_code= 201) #status_code define o tipo de status que queremos ao executar
+@app.post("/blog", status_code=status.HTTP_201_CREATED) #status_code define o tipo de status que queremos ao executar
 def create(request: schemas.Blog, db: Session = Depends(get_db)):
     new_blog = models.Blog(title=request.title, body=request.body)
     db.add(new_blog)
@@ -35,10 +34,40 @@ def all(db: Session = Depends(get_db)):
     return blogs
 
 
-@app.get("/blog/{id}") #pegando um determinado dados pela id
-def show(id, db: Session = Depends(get_db)):
+@app.get("/blog/{id}", status_code=200) #pegando um determinado dados pela id
+def show(id, response: Response, db: Session = Depends(get_db)):
     blog = db.query(models.blog).filter(models.Blog.id == id).first # filter funciona como um "where" do sql / first significa que queremos apenas o primeiro
+    if not blog:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail= f"O blog de número {id}, não está disponivel.") #erro e mensagem em uma linha
+        # response.status_code = status.HTTP_404_NOT_FOUND # usando o response para colocar uma condição que dira um novo status se ativada
+        #return {"detalhe" : f"O blog de número {id}, não está disponivel." } 
     return blog
+
+
+@app.delete("/blog/{id}", status_code=status.HTTP_204_NO_CONTENT)
+def destroy(id, db: Session = Depends(get_db)):
+    blog = db.query(models.Blog).filter(models.Blog.id == id)
+    if not blog.first():
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Blog com a id {id}, não encontrado")
+
+    blog.delete(synchronize_session=False)#delete pego na documentação do alchemy
+    db.commit() #sempre que fazer uma alteração no banco de dados vc tem que commitar ela
+    return "done"
+
+
+@app.put("/blog/{id}", status_code=status.HTTP_202_ACCEPTED)
+def update(id, request: schemas.Blog, db: Session = Depends(get_db)):
+    blog = db.query(models.Blog).filter(models.Blog.id == id)
+    if not blog.first():
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Blog com a id {id}, não encontrado")
+
+
+    blog.update(request)
+    db.commit()   #SEMPRE PRECISAMOS COMMITAR PARA FAZER UMA ALTERAÇÃO NO CODIGO
+    return "updated"
+
+
+
 
 
 if __name__ == "__main__":
