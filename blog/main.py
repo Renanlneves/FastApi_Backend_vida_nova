49b330397d087b09b7b7ba80b333e42dce27d64b
renanlneves <renanlneves@gmail.com>
2022-11-03 19:14:25 -0300

    adicionadas rotas

--- blog/main.py
+++ blog/main.py
@@ -17,20 +17,5 @@ models.Base.metadata.create_all(bind=engine)
 app.include_router(blog.router)
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 if __name__ == "__main__":
     uvicorn.run("main:app", host="127.0.0.1", port=8038,reload=True)
