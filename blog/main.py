ef15afc76a601aac561913737905d2d015f7fa44
Renan Neves <renanlneves@gmail.com>
2022-11-03 21:49:17 -0300

    adicionadas rotas

--- blog/main.py
+++ blog/main.py
@@ -1,13 +1,8 @@
-from typing import List
-from fastapi import FastAPI, Depends, status, Response, HTTPException
+from fastapi import FastAPI
 import uvicorn
-from config.database import SessionLocal
-from domain.account import schemas
 from domain.account import models
-from config.database import engine, get_db
-from sqlalchemy.orm import Session
-from domain.account.hashing import Hash
-from routers import blog
+from config.database import engine
+from routers import blog, user
 
 app = FastAPI()
 
@@ -15,7 +10,7 @@ app = FastAPI()
 models.Base.metadata.create_all(bind=engine)
 
 app.include_router(blog.router)
-
+app.include_router(user.router)
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="127.0.0.1", port=8038,reload=True)
